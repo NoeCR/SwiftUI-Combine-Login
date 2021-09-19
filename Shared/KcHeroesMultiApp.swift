c185b06774052b45045795d878467952919b4ae3
NoeCR <noecr.dev@gmail.com>
2021-09-19 09:08:55 +0200

    Initial Commit

---
+++ Shared/KcHeroesMultiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KcHeroesMultiApp.swift
+//  Shared
+//
+//  Created by Noe Cruz Rodriguez on 19/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct KcHeroesMultiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
